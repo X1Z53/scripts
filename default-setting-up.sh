1c6cd4982094734bf73892bcef9951f7398d1531
x1z53 <x1z53@yandex.ru>
2024-03-05 01:42:24 +0300

    Fixes, aliases

--- default-setting-up.sh
+++ default-setting-up.sh
@@ -2,19 +2,20 @@
 
 set -xe
 
-# Default things
-sudo dnf update -y
+# Usual stuff
 sudo dnf remove -y gnome-abrt gnome-boxes gnome-characters gnome-connections gnome-contacts gnome-font-viewer gnome-logs gnome-maps gnome-system-monitor gnome-text-editor gnome-tour cheese rhythmbox simple-scan totem mediawriter libreoffice-calc libreoffice-writer libreoffice-impress yelp firefox nano
 sudo dnf install -y vim git
 hostnamectl set-hostname X1Z53
 rm -rf ~/Music/ ~/Public/ ~/Desktop/ ~/Templates/
 rm -rf ~/Музыка/ ~/Общие/ ~/Рабочий\ стол/ ~/Шаблоны/
-sudo rm /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo /etc/yum.repos.d/google-chrome.repo /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo /etc/yum.repos.d/rpmfusion-nonfree-steam.repo /etc/yum.repos.d/fedora-cisco-openh264.repo
+
+# Repositories
+sudo rm -r /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo /etc/yum.repos.d/google-chrome.repo /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo /etc/yum.repos.d/rpmfusion-nonfree-steam.repo /etc/yum.repos.d/fedora-cisco-openh264.repo
 sudo flatpak remote-delete fedora
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remote-modify --enable flathub
 
-# Install FiraCode Nerd Font
+# FiraCode Nerd Font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
 unzip FiraCode.zip -d ttf
 mkdir -p ~/.local/share/fonts
@@ -22,39 +23,50 @@ mv ttf/*ttf ~/.local/share/fonts/
 fc-cache -f -v
 rm -rf ttf FiraCode.zip
 
-# GNOME settings
-sudo dnf install -y gnome-themes-extra # gnome-tweaks
+# Gsettings
+sudo dnf install -y gnome-themes-extra
 gsettings set org.gnome.desktop.input-sources xkb-options "['']"
-gsettings set org.gnome.desktop.interface document-font-name 'FiraCode Nerd Font Regular 10'
-gsettings set org.gnome.desktop.interface font-name 'FiraCode Nerd Font Regular 10'
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface document-font-name "FiraCode Nerd Font Regular 10"
+gsettings set org.gnome.desktop.interface font-name "FiraCode Nerd Font Regular 10"
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-gsettings set org.gnome.desktop.interface monospace-font-name 'FiraCode Nerd Font Regular 10'
+gsettings set org.gnome.desktop.interface monospace-font-name "FiraCode Nerd Font Regular 10"
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'FiraCode Nerd Font Regular 10'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "FiraCode Nerd Font Regular 10"
 gsettings set org.gnome.mutter center-new-windows true
 gsettings set org.gnome.mutter dynamic-workspaces false
 
-# Reinstall apps
+# Flatpak
 sudo dnf remove -y baobab gnome-terminal evince gnome-clocks loupe gnome-weather 
 sudo dnf install -y qterminal
-flatpak install -y flathub org.gnome.baobab md.obsidian.Obsidian io.github.tdesktop_x64.TDesktop org.libreoffice.LibreOffice org.kde.krita org.kde.kdenlive com.github.tchx84.Flatseal com.mattjakeman.ExtensionManager io.missioncenter.MissionCenter io.github.Rirusha.Cassette org.gnome.Evince org.gnome.clocks org.gnome.Loupe org.gnome.weather
+flatpak install -y flathub org.gnome.baobab md.obsidian.Obsidian io.github.tdesktop_x64.TDesktop org.libreoffice.LibreOffice org.kde.krita org.kde.kdenlive com.github.tchx84.Flatseal com.mattjakeman.ExtensionManager io.missioncenter.MissionCenter io.github.Rirusha.Cassette org.gnome.Evince org.gnome.clocks org.gnome.Loupe org.gnome.Weather
 
-# Git settings
+# Git
+git config --global core.editor "nvim"
 git config --global user.name "x1z53"
 git config --global user.email "x1z53@yandex.ru"
 git config --global credential.helper store
 
-# Install Yandex Browser
+# Yandex Browser
 sudo rpmkeys --import https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG
 sudo dnf config-manager --add-repo http://repo.yandex.ru/yandex-browser/rpm/beta/x86_64
 sudo dnf install -y yandex-browser-beta
 sudo /opt/yandex/browser-beta/update_codecs /opt/yandex/browser-beta
+rm ~/yandex-browser-beta.desktop
 
-# Install AstroNvim
+# AstroNvim
 sudo dnf install -y neovim gcc
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
+# QTerminal
+sed -i "s/HideTabBarWithOneTab=false/ideTabBarWithOneTab=true/g"         ~/.config/qterminal.org/qterminal.ini
+sed -i "s/HistoryLimited=true/istoryLimited=false/g"                     ~/.config/qterminal.org/qterminal.ini
+sed -i "s/MenuVisible=true/enuVisible=false/g"                           ~/.config/qterminal.org/qterminal.ini
+sed -i "s/ScrollbarPosition=2/crollbarPosition=0/g"                      ~/.config/qterminal.org/qterminal.ini
+sed -i "s/highlightCurrentTerminal=true/ighlightCurrentTerminal=false/g" ~/.config/qterminal.org/qterminal.ini
+sed -i "s/fontFamily=Monospace/fontFamily=FiraCode Nerd Font/g"          ~/.config/qterminal.org/qterminal.ini
 
 # Install ZSH
 sudo dnf install -y zsh
--- zsh-configure.sh
+++ zsh-configure.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
+
+set -xe
+
+# PowerLevel10k
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+
+# Plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=( git zsh-syntax-highlighting zsh-autosuggestions )/g' ~/.zshrc
-echo "Copy and do yourself:"
-echo "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k"
+
+# Useful stuff (aliases)
+sudo dnf install -y exa zoxide
+echo "eval \"\$(zoxide init zsh)\"" >> ~/.zshrc
+echo "alias cd=\"z\""               >> ~/.zshrc
+echo "alias ls=\"exa\""             >> ~/.zshrc
+
+echo ""
+echo "Do it yourself:"
+echo "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \$ZSH_CUSTOM/themes/powerlevel10k"
 
